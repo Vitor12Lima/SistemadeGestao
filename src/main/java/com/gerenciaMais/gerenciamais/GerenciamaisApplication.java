d604cd02fb6c9b86622809cc6574b58964ab1fe3
Vitor12Lima <vitorlimarodrigues10@gmail.com>
2018-10-23 09:11:26 -0300

    inicializando o projeto (vitor, eliabe, debora)

---
+++ src/main/java/com/gerenciaMais/gerenciamais/GerenciamaisApplication.java
@@ -0,0 +1,12 @@
+package com.gerenciaMais.gerenciamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamaisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gerenciaMais/gerenciamais/GerenciamaisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gerenciaMais.gerenciamais;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciamaisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
