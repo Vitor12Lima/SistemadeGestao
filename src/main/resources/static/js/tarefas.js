ad17901a73eca80095c9f7d612772d9509378b81
debora-dams <deboraaurea7@gmail.com>
2018-12-14 13:03:03 -0300

    adição da função criarTabelaTarefas() e cadastrarTarefa() no arquivo tarefas.js

--- src/main/resources/static/js/tarefas.js
+++ src/main/resources/static/js/tarefas.js
@@ -1,28 +1,77 @@
+//////////////////////////////////////
+
+function cadastrarTarefa(){
+	
+	let tarefa = {
+		"dataInicial": document.getElementById("firstName").value,
+		"dataTermino": document.getElementById("lastName").value,
+		"descricao": document.getElementById("comment").value
+	};
+	
+	fetch("/tarefa", {
+		
+		method: "POST",
+		headers: {
+			"content-type": "application/JSON"
+		},
+		
+		body: JSON.stringify(tarefa)
+		
+	}). then(function(response){
+		
+		criarTabelaTarefas();
+		
+	}).catch(function(error){
+		console.log(error);
+	});
+	
+}
+
+//////////////////////////////////////
+function atualizarTarefa(){
+	
+}
+
+//////////////////////////////////////
+function apagarTarefa(){
+	
+	
+	
+}
 
 //////////////////////////////////////
 function criarTabelaTarefas(){
 	
-	fetch('/tarefa').then(function(response){
+	fetch("/tarefa"). then(function(response){
 		
 		if(response.status >= 200 && response.status <= 300){
 			
-			response.json().then(function(data)){
+			response.json(). then(function(data){
 				
-				let tabela = document.getElementById("usuario");
-				tabela.innerHTML = '';
+				let tabTarefa = document.getElementById("tarefas");
 				
-				for(let i = 0; i < data.content.lenght; i++){
-					let b = data.content[i];
-					tabela.innerHTML += 
-				}
+				tabTarefa.innerHTML = '';
 				
-				
+				tabTarefa.innerHTML = "<tr><td></td><td></td><td></td><td></td></tr>";
+					
+					for(let i = 0 ; i < data.content.length ; i++){
+						let b = data.content[i];
+						
+						tabTarefa.innerHTML += `<tr><td>${b.id}</td><td>${b.dataInicial}</td><td>${b.dataTermino}</td><td>${b.descricao}</td></tr>`;
+						
+					}
 				
 			})
 			
-		}).catch(function(error){
+		}
+		
+	}).catch(function(error){
 		console.log(error);
 	});
 	
-	
 }
+
+//////////////////////////////////////
+
+criarTabelaTarefas();
+
