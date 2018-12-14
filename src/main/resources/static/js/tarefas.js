d758b4b52e6425a3e5472c415f01ab3d121b8ed6
Vitor12Lima <vitorlimarodrigues10@gmail.com>
2018-12-14 10:57:08 -0300

    criação da folha de estilo tarefas.js

--- src/main/resources/static/js/tarefas.js
+++ src/main/resources/static/js/tarefas.js
@@ -2,6 +2,27 @@
 //////////////////////////////////////
 function criarTabelaTarefas(){
 	
+	fetch('/tarefa').then(function(response){
+		
+		if(response.status >= 200 && response.status <= 300){
+			
+			response.json().then(function(data)){
+				
+				let tabela = document.getElementById("usuario");
+				tabela.innerHTML = '';
+				
+				for(let i = 0; i < data.content.lenght; i++){
+					let b = data.content[i];
+					tabela.innerHTML += 
+				}
+				
+				
+				
+			})
+			
+		}).catch(function(error){
+		console.log(error);
+	});
 	
 	
 }
