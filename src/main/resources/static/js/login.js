6be0bbced2716f8443807be841ea4127f54d95cc
Vitor12Lima <vitorlimarodrigues10@gmail.com>
2018-12-14 10:55:02 -0300

    criação da função login2

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -16,13 +16,46 @@ function verificarLogin() {
 						
 						console.log(localStorage);
 						
-						alert("usuario" + `${a.id} + " - " + localStorage);
+						//alert("usuario: " + `${a.id}` + " - " + localStorage);
 						document.location = 'index.html';
 					}
 				}
 			});
 			
 		}
+		
+	}).catch(function(error){
+		console.log(error);
+	});
+	
+}
+
+function login2(){
+	
+	let email = document.getElementById('inputEmail').value;
+	let senha = document.getElementById('inputPassword').value;
+	
+	let data = {
+		"email": email,
+		"senha": senha
+	};
+	
+	fetch("/login",{
+		method: "POST",
+		headers: {
+			"Content-Type": "application/JSON"
+		},
+		body: JSON.stringify(data)
+	}).then(function(response){
+		
+		response.json().then(function(usuario){
+			
+			localStorage.setItem("usuario", usuario.id);
+			
+			console.log(localStorage);
+			document.location = 'index.html';
+			
+		});
 		
 	}).catch(function(error){
 		console.log(error);
