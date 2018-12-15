6ed3b889b10896b9e6076ee7363f9aac275d10e5
debora-dams <deboraaurea7@gmail.com>
2018-12-14 22:46:55 -0300

    modificação no objeto usuario da função cadastrarUsuario() no arquivo main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,30 +2,26 @@
 //////////////////////////////////////
 
 function cadastrarUsuario(){
-	
-	let usuario = {"nome": document.getElementById("nome").value, 
-			"email": document.getElementById("email").value,
-			"senha": document.getElementById("senha").value};
-
+	let usuario = {"nome": document.getElementById("firstName").value, 
+			"email": document.getElementById("inputEmail").value,
+			"senha": document.getElementById("inputPassword").value};
 	
 	fetch("/usuario", {
-		
 		method: "POST",
 		headers: {
 			"content-type": "application/JSON"
 		},
 		
 		body: JSON.stringify(usuario)
-	}). then(function (response){
+			
+	}). then(function(response){
 		
 		criarTabela();
+		document.location = 'index.html';
 		
 	}).catch(function (error){
-		
 		console.log(error);
-		
 	});
-	
 }
 
 //////////////////////////////////////
